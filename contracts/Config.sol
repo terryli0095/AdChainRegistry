20d5f5ea9ff603c789130cca271ebdb0876758c2
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-06-19 11:07:12 -0400

    Add state variables for configuration

--- contracts/Config.sol
+++ contracts/Config.sol
@@ -1,3 +1,22 @@
 contract Config {
+	/// application & challenger deposit amounts
+	uint public minDeposit; 
+	/// duration of the challenge period
+	uint public challengeDuration;
+	/// duration of a registration’s validity
+	uint public registryDuration;
+	/// duration of the commit period in token votes
+	uint public commitVoteDuration;
+	/// duration of reveal period in token votes 
+	uint public revealVoteDuration;
+	/// percentage of forfeited deposit distributed to winning party
+	uint public dispensationRatio;
+
+	/// share of tokens required to initiate a reparameterization
+	uint public initialProposalThreshold
+	/// num votes needed to pass a reparameterization 
+	uint public votesQuota;
 	
+	/// maps proposal hashes to corresponding vote tallies
+	mapping(bytes32 => uint) public proposalMap;
 }
